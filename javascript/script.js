24f31becdab9d629f8da016c4847bdde6b5e151d
Maria Racariu <90383391+MariaRacariu@users.noreply.github.com>
2023-11-08 14:20:59 +0100

    wrote instructions for javascript

---
+++ javascript/script.js
@@ -0,0 +1,31 @@
+// FIRST PART
+// loop that outputs 5 texts
+// the texts need to go up in size, seams like it's double
+// The text needs to change color from light green to light blue
+// The text needs to be centered
+
+// SECOND PART
+// A div with a black solid border around
+// The div needs three columsn and they need space around
+// Need an array with numbers 0-9 witch will be used twice
+// Need an array with the numbers 1-10 in text format
+
+// ------First column ----------
+// Print number array from 0-9
+// The column background needs to be purpel
+// Each number row needs to be white or black
+// The numbers need to be on the left side of the row container
+// Number 4 needs to have the baclground purpel
+
+// ------Second column ----------
+// Print number array from 0-9 in reverse
+// The column background needs to be purpel
+// Each number row needs to be white or black but in reverse from the first row
+// The numbers need to be centered in the row container
+// Number 8 needs to have the background purple
+
+// ------Third column ----------
+// Print number text array from 1-10
+// The column background needs to be purpel
+// Each number needs to be on the right side of the row container
+// Number 6 needs to have the background purple
